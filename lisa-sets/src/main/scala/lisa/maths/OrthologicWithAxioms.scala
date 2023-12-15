838c0a27fe4a839a65e17590cfc7bb7bb82e8616
Eugene Flesselle <eugene@flesselle.net>
2023-12-15 18:25:04 +0100

    before introducing symbol for encoding as sequent

--- lisa-sets/src/main/scala/lisa/maths/OrthologicWithAxioms.scala
+++ lisa-sets/src/main/scala/lisa/maths/OrthologicWithAxioms.scala
@@ -78,8 +78,6 @@ object OrthologicWithAxioms extends lisa.Main:
     )
   }
 
-//  val leftOr =
-
   val leftAnd1 = Theorem((x <= z) ==> ((x n y) <= z)) {
     have(thesis) by Tautology.from(
       p4a,
@@ -92,7 +90,27 @@ object OrthologicWithAxioms extends lisa.Main:
   val leftOr1 = Theorem((x <= z) /\ (y <= z) ==> ((x u y) <= z)):
     have(thesis) by Restate.from(p6b)
 
-//  val leftNot1 = Theorem((x <= y) ==> ())
+
+  val notEquiv = Theorem((x <= not(y)) <=> (y <= not(x))) {
+    val dir1 = have((x <= not(y)) ==> (y <= not(x))) subproof {
+      have(thesis) by Tautology.from(
+        p8 of (y := not(y)), // (x <= not(y)) ==> (not(not(y)) <= not(x))
+        p7a of (x := y), // y <= not(not(y))
+        p2 of (x := y, y := not(not(y)), z := not(x))
+      )
+    }
+    have(thesis) by Tautology.from(dir1, dir1 of (x := y, y := x))
+  }
+
+  val p8Cons = Theorem((not(y) <= not(x)) ==> (x <= y)) {
+    have(thesis) by Tautology.from(
+      p8 of (x := not(y), y := not(x)), // not(not(x)) <= not(not(y))
+      p7a, // x <= not(not(x))
+      p7b of (x := y), // not(not(y)) <= y
+      p2 of (y := not(not(x)), z := not(not(y))),
+      p2 of (y := not(not(y)), z := y),
+    )
+  }
 
 
   object RestateWithAxioms extends ProofTactic:
@@ -113,149 +131,233 @@ object OrthologicWithAxioms extends lisa.Main:
       enum AnnotatedFormula:
         case L(f: Term)
         case R(f: Term)
-        case NoAnnotatedFormula
+        case NoFormula
       import AnnotatedFormula.*
-      type SomeAnnotatedFormula = L | R
+      type SomeFormula = L | R
 
 
-      case class Done(conlusion: proof.ProofTacticJudgement)
-      case class Opt1(prem: (Term, Term), proofFromPrem: proof.ProofStep => proof.ProofTacticJudgement)
-      case class Opt2(prem: (Term, Term), proofFromPrem: proof.ProofStep => proof.ProofTacticJudgement)
+      case class Done(conlusion: Any)
+      case class Opt1(prem: (AnnotatedFormula, AnnotatedFormula), proofFromPrem: proof.ProofStep => Any)
+      case class Opt2(prem1: (AnnotatedFormula, AnnotatedFormula), prem2: (AnnotatedFormula, AnnotatedFormula), proofFromPrem: (proof.ProofStep, proof.ProofStep) => Any)
       type Opt = Done | Opt1 | Opt2
 
-      /*
-      case class OlSequent(f1: AnnotatedFormula, f2: AnnotatedFormula):
-        def underlying: Sequent =
-          val left = Seq(f1, f2) collect { case L(f) => f }
-          ???
-      */
 
-//      extension (j: proof.ProofTacticJudgement)
-//        def map(f: proof.ProofStep => proof.ProofStep): proof.ProofTacticJudgement =
-//          j.validate
-//          j match
-//            case
-//          ???
+      def solveA(gamma: AnnotatedFormula, delta: AnnotatedFormula): proof.ProofTacticJudgement = TacticSubproof:
 
-      /* () |- left <= right */
-      def solveLR(left: Term, right: Term): proof.ProofTacticJudgement = TacticSubproof:
+        (gamma, delta) match
 
-
-        val opts = (left, right) match
-
-          case (l, r) if l == r =>
-            Seq(
-              () => have(l <= r) by Restate.from(p1 of (x := l))
-            )
-
-          case (left, right) =>
-            def op1 =
-              val s1 = solveL(left)
-              if !s1.isValid then s1
-              else
-                have(left <= right) by Tautology.from(
-                  have(s1),
-                  p3a of (x := right),
-                  p2 of (x := left, y := zero, z := right)
-                )
-
-
-            def opt2 = ???
-
-            Seq(op1, opt2)
-
-        ???
-      end solveLR
-
-      def solveLL(left: Term, right: Term): proof.ProofTacticJudgement = ???
-      def solveRR(left: Term, right: Term): proof.ProofTacticJudgement = ???
-
-      /* () |- left <= 0 */
-      def solveL(left: Term): proof.ProofTacticJudgement = ???
-      def solveR(right: Term): proof.ProofTacticJudgement = ???
-
-
-
-
-//       TODO
-//      val proven, visited: Set[(AnnotatedFormula, AnnotatedFormula)] = mMap.empty
-
-      def solve(left: Term, right: Term): proof.ProofTacticJudgement =
-
-//        val opts: Seq[proof.ProofTacticJudgement | Opt] = (left, right) match
-        val opts = (left, right) match
-
-          case (l, r) if l == r =>
-            have(l <= r) by Restate.from(p1 of (x := l))
-
-//          case (l, r) if axioms // TODO
-
-          case (not(l), r) => ???
-
-          case (n(a, b), r) => Seq(
-//            Opt(prems = (a, r), proofFromPrem = (s: proof.ProofStep) =>
+          // Weaken
+//          case (gamma: SomeFormula, delta: SomeFormula) => Seq(
+//            Opt1(prem = (gamma, NoFormula), proofFromPrem = s =>
 //              have(left <= right) by Tautology.from(
-//                p4a of (x := a, y := b), // (a n b) <= a
-//                s, // a <= r
-//                p2 of (x := (a n b), y := a, z := r)
+//                s, // left <= 0
+//                p3a of (x := right), // 0 <= right
+//                p2 of (x := left, y := zero, z := right)
 //              )
 //            ),
-//            Opt(prems = (b, r), proofFromPrem = (s: proof.ProofStep) =>
+//            Opt1(prem = (NoFormula, R(right)), proofFromPrem = s =>
 //              have(left <= right) by Tautology.from(
-//                p5a of (x := a, y := b), // (a n b) <= b
-//                s, // b <= r
-//                p2 of (x := (a n b), y := b, z := r)
+//                s, // 1 <= right
+//                p3b of (x := left), // left <= 1
+//                p2 of(x := left, y := one, z := right)
 //              )
 //            )
-          )
+//          )
 
-          case (u(a, b), r) =>
+          /****** L R ******/
 
-            ???
+          case (L(left), R(right)) => (left, right) match
 
-
-        ???
-      end solve
-
-      def solveA(left: AnnotatedFormula, right: AnnotatedFormula): proof.ProofTacticJudgement =
-
-        // TODO assuming ordered by L, R, None
-        val goal: Formula = (left, right) match
-          case (L(f1), R(f2)) => f1 <= f2
-          case (L(f1), L(f2)) => f1 <= not(f2)
-          case (R(f1), R(f2)) => not(f1) <= f2
-          case (L(f1), None) => f1 <= zero
-          case (R(f1), None) => one <= f1
-
-        val opts: Seq[Opt] = ???
-
-        def solveWithHyp = (left, right) match
-          case (L(f1), R(f2)) if f1 == f2 =>
-            Done(have(f1 <= f2) by Tautology.from(p1))
-          case (R(f1), L(f2)) if f1 == f2 =>
-            Done(have(f2 <= f1) by Tautology.from(p1))
-
-        def solveWithWeaken = (left, right) match
-          case (left: SomeAnnotatedFormula, right: SomeAnnotatedFormula) =>
-
-            Opt1(
-              prem = (left, NoAnnotatedFormula),
-              proofFromPrem = (s: proof.ProofStep) =>
-                have(goal) by Tautology.from(
-                  ???
-                )
+            case (left, right) if left == right => Seq(
+              Done(have(left <= right) by Restate.from(p1 of (x := left)))
             )
 
-            ???
+            case (left, right) => Seq(
+              Opt1(prem = (L(left), NoFormula), proofFromPrem = s =>
+                have(left <= right) by Tautology.from(
+                  s, // left <= 0
+                  p3a of (x := right), // 0 <= right
+                  p2 of (x := left, y := zero, z := right)
+                )
+              ),
+              Opt1(prem = (NoFormula, R(right)), proofFromPrem = s =>
+                have(left <= right) by Tautology.from(
+                  s, // 1 <= right
+                  p3b of (x := left), // left <= 1
+                  p2 of(x := left, y := one, z := right)
+                )
+              )
+            )
 
-        def solveWithLeftNot = (left, right) match
+            case (not(left), right) => Seq(
+              Opt1(prem = (R(left), R(right)), proofFromPrem = s =>
+                have(not(left) <= right) by Restate.from(s)
+              )
+            )
 
-          case (L(not(f1)), r) =>
-            val prem = (R(f1), r)
-            val proofFromPrem =
+            case (n(l1, l2), right) => Seq(
+              Opt1(prem = (L(l1), R(right)), proofFromPrem = s =>
+                have((l1 n l2) <= right) by Tautology.from(
+                  s, // l1 <= right
+                  p4a of (x := l1, y := l2), // (l1 n l2) <= l1
+                  p2 of (x := (l1 n l2), y := l1, z := right)
+                )
+              ),
+              Opt1(prem = (L(l2), R(right)), proofFromPrem = s =>
+                have((l1 n l2) <= right) by Tautology.from(
+                  s, // l2 <= right
+                  p5a of (x := l1, y := l2), // (l1 n l2) <= l2
+                  p2 of (x := (l1 n l2), y := l2, z := right)
+                )
+              )
+            )
+
+            case (u(l1, l2), right) => Seq(
+              Opt2(prem1 = (L(l1), R(right)), prem2 = (L(l2), R(right)), proofFromPrem = (s1, s2) =>
+                have((l1 u l2) <= right) by Tautology.from(
+                  s1, // l1 <= right
+                  s2, // l2 <= right
+                  p6b of(x := l1, y := l2, z := right)
+                )
+              )
+            )
+
+            case (left, not(right)) => Seq(
+              Opt1(prem = (L(left), L(right)), proofFromPrem = s =>
+                have(left <= not(right)) by Restate.from(s)
+              )
+            )
+
+            case (left, n(r1, r2)) => Seq(
+              Opt2(prem1 = (L(left), R(r1)), prem2 = (L(left), R(r2)), proofFromPrem = (s1, s2) =>
+                have(left <= (r1 n r2)) by Tautology.from(
+                  s1, // left <= r1
+                  s2, // left <= r2
+                  p6a of(x := left, y := r1, z := r2)
+                )
+              )
+            )
+
+            case (left, u(r1, r2)) => Seq(
+              Opt1(prem = (L(left), R(r1)), proofFromPrem = s =>
+                have(left <= (r1 n r2)) by Tautology.from(
+                  s, // left <= r1
+                  p4b of(x := r1, y := r2), // r1 <= (r1 u r2)
+                  p2 of(x := left, y := r1, z := (r1 u r2))
+                )
+              ),
+              Opt1(prem = (L(left), R(r2)), proofFromPrem = s =>
+                have(left <= (r1 n r2)) by Tautology.from(
+                  s, // left <= r2
+                  p5b of(x := r1, y := r2), // r2 <= (r1 u r2)
+                  p2 of(x := left, y := r2, z := (r1 u r2))
+                )
+              ),
+            )
+
+          /****** L(left1), L(left2) --> left1 <= not(left2) ******/
+
+          case (L(left1), L(left2)) => (left1, left2) match
+
+            case (left1, left2) => Seq(
+              Opt1(prem = (L(left1), NoFormula), proofFromPrem = s =>
+                have(left1 <= not(left2)) by Tautology.from(
+                  s, // left1 <= 0
+                  p3a of (x := not(left2)), // 0 <= not(left2)
+                  p2 of (x := left1, y := zero, z := not(left2))
+                )
+              ),
+              Opt1(prem = (L(left2), NoFormula), proofFromPrem = s =>
+                val s2 = have(left2 <= not(left1)) by Tautology.from(
+                  s, // left2 <= 0
+                  p3a of (x := not(left1)), // 0 <= not(left1)
+                  p2 of (x := left2, y := zero, z := not(left1))
+                )
+                have(left1 <= not(left2)) by Tautology.from(
+                  s2, //
+                  notEquiv of (x := left1, y := left2)
+                )
+              ),
+            )
+
+            case (not(left1), left2) => Seq(
+              Opt1(prem = (R(left1), L(left2)), proofFromPrem = s =>
+                have(not(left1) <= not(left2)) by Tautology.from(
+                  s, // left2 <= left1
+                  p8Cons of (x := left2, y := left1)
+                )
+              )
+            )
+
+            // AR was same as L R
+            case (n(l1, l2), left2) => Seq(
+              Opt1(prem = (L(l1), L(left2)), proofFromPrem = s =>
+                have((l1 n l2) <= not(left2)) by Tautology.from(
+                  s, // l1 <= not(left2)
+                  p4a of (x := l1, y := l2), // (l1 n l2) <= l1
+                  p2 of (x := (l1 n l2), y := l1, z := not(left2))
+                )
+              ),
+              Opt1(prem = (L(l2), L(left2)), proofFromPrem = s =>
+                have((l1 n l2) <= not(left2)) by Tautology.from(
+                  s, // l2 <= not(left2)
+                  p5a of (x := l1, y := l2), // (l1 n l2) <= l2
+                  p2 of (x := (l1 n l2), y := l2, z := not(left2))
+                )
+              )
+            )
+
+            // AR was same as L R
+            case (u(l1, l2), left2) => Seq(
+              Opt2(prem1 = (L(l1), L(left2)), prem2 = (L(l2), L(left2)), proofFromPrem = (s1, s2) =>
+                have((l1 u l2) <= not(left2)) by Tautology.from(
+                  s1, // l1 <= not(left2)
+                  s2, // l2 <= not(left2)
+                  p6b of(x := l1, y := l2, z := not(left2))
+                )
+              )
+            )
+
+            case (left1, not(left2)) => Seq(
+              Opt1(prem = (L(left1), R(left2)), proofFromPrem = s =>
+                have(left1 <= not(not(left2))) by Tautology.from(
+                  s, // left1 <= left2
+                  p7a of (x := left2), // left2 <= not(not(left2))
+                  p2 of (x := left1, y := left2, z := not(not(left2)))
+                )
+              )
+            )
+
+            case (left1, n(l1, l2)) => Seq(
+              Opt2(prem1 = (L(left1), R(l1)), prem2 = (L(left1), R(l2)), proofFromPrem = (s1, s2) =>
+                have(left1 <= (l1 n l2)) by Tautology.from(
+                  s1, // left <= r1
+                  s2, // left <= r2
+                  p6a of(x := left1, y := l1, z := l2)
+                )
+              )
+            )
+
+            case (left, u(r1, r2)) => Seq(
+              Opt1(prem = (L(left), R(r1)), proofFromPrem = s =>
+                have(left <= (r1 n r2)) by Tautology.from(
+                  s, // left <= r1
+                  p4b of(x := r1, y := r2), // r1 <= (r1 u r2)
+                  p2 of(x := left, y := r1, z := (r1 u r2))
+                )
+              ),
+              Opt1(prem = (L(left), R(r2)), proofFromPrem = s =>
+                have(left <= (r1 n r2)) by Tautology.from(
+                  s, // left <= r2
+                  p5b of(x := r1, y := r2), // r2 <= (r1 u r2)
+                  p2 of(x := left, y := r2, z := (r1 u r2))
+                )
+              ),
+            )
 
 
-            ???
+//          case (L(left1), L(left2)) => (left1, left2)
+
 
         ???
       end solveA
